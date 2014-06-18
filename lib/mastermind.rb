c7a8839bca979603b7bf6cb40257d7909ee5f302
Andy Mention <amention@gmail.com>
2014-06-18 14:44:46 -0600

    Refactors the handling of q/quit instructions

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -9,12 +9,16 @@ class Repl
 
   def initialize
     puts "Initializing"
-    @input = ''
+  end
+
+  def wants_to_quit?(input)
+    input == 'q' || input == 'quit'
   end
 
   def start
     puts "Welcome to MASTERMIND \n Is your mind masterful?"
-    unless @input == ('q' || 'quit')
+    input = ''
+    unless wants_to_quit?(input)
       puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
       input = gets.strip
       case input
@@ -27,7 +31,6 @@ class Repl
 
   def instructions
     puts "Mastermind is a game of wits. You must successfully guess the code the secret code. On each guess, you will learn the number of correct colors guessed as well as the number placed in the correct slots."
-    return
   end
   
   def start_new_game
@@ -37,17 +40,21 @@ class Repl
   end
   
   def play
-    while @input != 'q'
-      system('clear')
-      puts "This sequence consists  of: (r)ed, (g)reen, (b)lue, and (y)ellow. \n Use q to quit at any time to end the game. Guess a sequence of four colors."
-      @input = gets.strip
-      @game.new_guess(@input)
-      puts "Guess number #{@game.turns}"
-      @game.guess_printer
-      # if @game.game_over?
-      #   game_over
-      # end
+    input = ''
+    
+    while !wants_to_quit?(input) && !@game.game_over?
+      puts "This sequence consists  of: (r)ed, (g)reen, (b)lue, and (y)ellow."
+      puts "Use q to quit at any time to end the game. Guess a sequence of four colors."
+      input = gets.strip
+      
+      unless wants_to_quit?(input)
+        @game.new_guess(input)
+        puts "Guess number #{@game.turns}"
+        @game.guess_printer
+      end
     end
+    
+    puts "The game is over."
   end
   
 end
