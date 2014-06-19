2883d7dc39f8c66188f7e0449337a55a544ba947
Andy Mention <amention@gmail.com>
2014-06-19 10:54:16 -0600

    creates more robust test coverage for sequence generator and sequence matcher

--- test/sequence_generator_test.rb
+++ test/sequence_generator_test.rb
@@ -24,4 +24,38 @@ class SequenceGeneratorTest < Minitest::Test
     s2_ary = s2.generate
     refute s1_ary.sequence == s2_ary.sequence
   end
+  
+  def test_it_can_make_a_medium_difficulty_sequence
+    sequence = SequenceGenerator.new(6, %w(r g b y c m))
+    sequence.generate
+
+    assert sequence.generate
+  end
+  
+  def test_it_makes_random_medium_difficulty_sequences
+    s1 = SequenceGenerator.new(6, %w(r g b y c m))
+    s2 = SequenceGenerator.new(6, %w(r g b y c m))
+    s1_ary = []
+    s2_ary = []
+    s1_ary = s1.generate
+    s2_ary = s2.generate
+    refute s1_ary.sequence == s2_ary.sequence
+  end
+  
+  def test_it_can_make_a_hard_difficulty_sequence
+    sequence = SequenceGenerator.new(8, %w(r g b y c m w k))
+    sequence.generate
+
+    assert sequence.generate
+  end
+  
+  def test_it_makes_random_hard_difficulty_sequences
+    s1 = SequenceGenerator.new(8, %w(r g b y c m w k))
+    s2 = SequenceGenerator.new(8, %w(r g b y c m w k))
+    s1_ary = []
+    s2_ary = []
+    s1_ary = s1.generate
+    s2_ary = s2.generate
+    refute s1_ary.sequence == s2_ary.sequence
+  end
 end
--- test/sequence_matcher_test.rb
+++ test/sequence_matcher_test.rb
@@ -22,6 +22,7 @@ class SequenceMatcherTest < Minitest::Test
     match = SequenceMatcher.new(guess, ['r','g','b','y'])
     assert_equal match.correct_color, 4
   end
+  
   def test_it_knows_correct_color2
     guess = Guess.new(%w(y y y y), 4)
     match = SequenceMatcher.new(guess, ['r','g','b','y'])
@@ -48,11 +49,51 @@ class SequenceMatcherTest < Minitest::Test
     assert_equal match.correct_color, 1
   end
 
-  def test_it_knows_correct_position
-    guess = Guess.new(%w(r g y b), 4)
+  def test_it_knows_correct_position_with_medium_difficulty
+    guess = Guess.new(%w(r g y b c m), 6)
   
-    match = SequenceMatcher.new(guess, ['r','g','b','y'])
+    match = SequenceMatcher.new(guess, ['r','g','b','y','r','y'])
 
     assert_equal match.correct_position, 2
+  end
+  
+  def test_it_knows_a_full_match_with_medium_difficulty
+    guess = Guess.new(%w(r g b y c m), 6)
+    match = SequenceMatcher.new(guess, ['r','g','b','y','c','m'])
+
+    assert match.full_match?
+
+    match = SequenceMatcher.new(guess, ['r','g','g','y','c','m'])
+    refute match.full_match?
+  end
+
+  def test_it_knows_correct_color_with_medium_difficulty
+    guess = Guess.new(%w(g r y b c m), 6)
+    match = SequenceMatcher.new(guess, ['r','g','b','y','m','c'])
+    assert_equal match.correct_color, 6
+  end
+  
+  def test_it_knows_correct_position_with_hard_difficulty
+    guess = Guess.new(%w(r g y b c m k w), 8)
+  
+    match = SequenceMatcher.new(guess, ['r','g','b','y','r','y','k','w'])
+
+    assert_equal match.correct_position, 4
+  end
+  
+  def test_it_knows_a_full_match_with_hard_difficulty
+    guess = Guess.new(%w(r g b y c m k w), 8)
+    match = SequenceMatcher.new(guess, ['r','g','b','y','c','m','k','w'])
+
+    assert match.full_match?
+
+    match = SequenceMatcher.new(guess, ['r','g','g','y','c','m','k','w'])
+    refute match.full_match?
+  end
+
+  def test_it_knows_correct_color_with_hard_difficulty
+    guess = Guess.new(%w(g r y b c m k w), 8)
+    match = SequenceMatcher.new(guess, ['r','g','b','y','m','c','g','g'])
+    assert_equal match.correct_color, 6
   end
 end
